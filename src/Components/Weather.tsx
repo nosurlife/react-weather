9c0f1b051ec8a5d032b48628dbd08776d76ec473
nosurlife <41213794+nosurlife@users.noreply.github.com>
2018-09-05 12:08:25 +1200

    Add files via upload

--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -9,7 +9,7 @@ class Weather extends React.Component<any, any> {
         return (
             <div>
                 { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
-                { this.props.teperature && <p>Temperature: { this.props.temperature }</p> }
+                { this.props.temperature && <p>Temperature: { this.props.temperature } Kelvin</p> }
                 { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
                 { this.props.description && <p>Conditions: { this.props.description }</p> }
                 { this.props.error && <p>{ this.props.error }</p> }
